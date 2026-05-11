f17951f8bdef4435ca26ffa53f231d994ab1d4ca
Ganesh Mali <ganeshmali.fc@gmail.com>
2026-05-11 14:06:44 +0530

    Update LSP_Violated.cs

--- LSP_Violated.cs
+++ LSP_Violated.cs
@@ -1,1 +1,15 @@
+public class Vehicle
+{
+    public virtual void StartEngine()
+    {
+        Console.WriteLine("Engine started");
+    }
+}
 
+public class Bicycle : Vehicle
+{
+    public override void StartEngine()
+    {
+        throw new Exception("No engine!");
+    }
+}
