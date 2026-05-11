e2f94acdf3cd295ec3cee9a7861c234287c141a3
Ganesh Mali <ganeshmali.fc@gmail.com>
2026-05-11 14:06:20 +0530

    Implement Vehicle class hierarchy with fuel calculation

--- OCP_Followed.cs
+++ OCP_Followed.cs
@@ -1,1 +1,22 @@
+public abstract class Vehicle
+{
+    public abstract double CalculateFuel();
+}
 
+public class Truck : Vehicle
+{
+    public override double CalculateFuel() => 100;
+}
+
+public class Bike : Vehicle
+{
+    public override double CalculateFuel() => 30;
+}
+
+public class FuelCalculator
+{
+    public double Calculate(Vehicle vehicle)
+    {
+        return vehicle.CalculateFuel();
+    }
+}
