cd948958aa13a5bc1c6f771d7312640b96eb7300
Ganesh Mali <ganeshmali.fc@gmail.com>
2026-05-11 14:07:49 +0530

    Update ISP_Followed.cs

--- ISP_Followed.cs
+++ ISP_Followed.cs
@@ -1,1 +1,23 @@
+public interface IDrivable
+{
+    void Drive();
+}
 
+public interface IFlyable
+{
+    void Fly();
+}
+
+public class Truck : IDrivable
+{
+    public void Drive()
+    {
+        Console.WriteLine("Truck driving");
+    }
+}
+
+public class Drone : IDrivable, IFlyable
+{
+    public void Drive() { }
+    public void Fly() { }
+}
