c4977c8499860304abd11615899585947d9629f5
Ganesh Mali <ganeshmali.fc@gmail.com>
2026-05-11 14:05:02 +0530

    Update SRP_Followed.cs

--- SRP_Followed.cs
+++ SRP_Followed.cs
@@ -1,1 +1,23 @@
+public class RouteService
+{
+    public void AssignRoute()
+    {
+        Console.WriteLine("Assigning route...");
+    }
+}
 
+public class CostService
+{
+    public void CalculateFuelCost()
+    {
+        Console.WriteLine("Calculating fuel cost...");
+    }
+}
+
+public class DeliveryRepository
+{
+    public void Save()
+    {
+        Console.WriteLine("Saving to DB...");
+    }
+}
