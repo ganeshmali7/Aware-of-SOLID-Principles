078d7ac963c345c240ad993494adf3795be2d5ff
Ganesh Mali <ganeshmali.fc@gmail.com>
2026-05-11 14:07:27 +0530

    Add IVehicle interface and Truck class implementation
    Implement IVehicle interface in Truck class with exception for Fly method.

--- ISP_Violated.cs
+++ ISP_Violated.cs
@@ -1,1 +1,15 @@
+public interface IVehicle
+{
+    void Drive();
+    void Fly();
+}
 
+public class Truck : IVehicle
+{
+    public void Drive() { }
+
+    public void Fly()
+    {
+        throw new Exception("Truck can't fly");
+    }
+}
