f1a9ed2ea7b9fc13ee2808dc3c7461ebf31edc11
Ganesh Mali <ganeshmali.fc@gmail.com>
2026-05-11 14:08:04 +0530

    Update DIP_Violated.cs

--- DIP_Violated.cs
+++ DIP_Violated.cs
@@ -1,1 +1,17 @@
+public class GPSService
+{
+    public void GetLocation()
+    {
+        Console.WriteLine("Getting location...");
+    }
+}
 
+public class Navigation
+{
+    private GPSService gps = new GPSService();
+
+    public void Start()
+    {
+        gps.GetLocation();
+    }
+}
