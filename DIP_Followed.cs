f0a384d26505065fe0b90f036f3f1ebd42419cbd
Ganesh Mali <ganeshmali.fc@gmail.com>
2026-05-11 14:08:20 +0530

    Update DIP_Followed.cs

--- DIP_Followed.cs
+++ DIP_Followed.cs
@@ -1,1 +1,27 @@
+public interface ILocationService
+{
+    void GetLocation();
+}
 
+public class GPSService : ILocationService
+{
+    public void GetLocation()
+    {
+        Console.WriteLine("Getting GPS location...");
+    }
+}
+
+public class Navigation
+{
+    private readonly ILocationService locationService;
+
+    public Navigation(ILocationService locationService)
+    {
+        this.locationService = locationService;
+    }
+
+    public void Start()
+    {
+        locationService.GetLocation();
+    }
+}
