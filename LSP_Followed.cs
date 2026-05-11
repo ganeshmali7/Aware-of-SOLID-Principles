4500fea94380a8a9b334ff880ff9136e5c39b310
Ganesh Mali <ganeshmali.fc@gmail.com>
2026-05-11 14:07:03 +0530

    Update LSP_Followed.cs

--- LSP_Followed.cs
+++ LSP_Followed.cs
@@ -1,1 +1,20 @@
+public interface IVehicle
+{
+}
 
+public interface IEngineVehicle : IVehicle
+{
+    void StartEngine();
+}
+
+public class Truck : IEngineVehicle
+{
+    public void StartEngine()
+    {
+        Console.WriteLine("Truck engine started");
+    }
+}
+
+public class Bicycle : IVehicle
+{
+}
