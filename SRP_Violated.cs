9827884a0852acba205c0836aa5568eb9f8740a9
Ganesh Mali <ganeshmali.fc@gmail.com>
2026-05-11 14:04:36 +0530

    Update SRP_Violated.cs

--- SRP_Violated.cs
+++ SRP_Violated.cs
@@ -1,1 +1,17 @@
+public class DeliveryService
+{
+    public void AssignRoute()
+    {
+        Console.WriteLine("Assigning route...");
+    }
 
+    public void CalculateFuelCost()
+    {
+        Console.WriteLine("Calculating fuel cost...");
+    }
+
+    public void SaveToDatabase()
+    {
+        Console.WriteLine("Saving to DB...");
+    }
+}
